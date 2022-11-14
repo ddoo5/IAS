f2cf39071c1769e61c50714f66915b6bbc6432c1
D <kritantablake@gmail.com>
2022-11-14 10:41:39 +0300

    Create DbDocsContext.cs

---
+++ DB/Context/DbDocsContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reflection.Metadata;
+using System.Xml.Linq;
+using IAS.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace IAS.DB.Context
+{
+	public class DbDocsContext : DbContext
+	{
+        public virtual DbSet<DocumentModel> Documents { get; set; }
+        public virtual DbSet<WordsModel> Words { get; set; }
+        public virtual DbSet<WordDocumentModel> WordDocuments { get; set; }
+
+
+        public DbDocsContext(DbContextOptions options)
+        {
+            //Database.EnsureDeleted();
+            //Database.EnsureCreated();
+        }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("DataSource=test.db;Cache=Shared");
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DocumentModel>();
+            modelBuilder.Entity<WordsModel>();
+            modelBuilder.Entity<WordDocumentModel>();
+        }
+    }
+}
+
