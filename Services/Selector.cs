7749dc9b05d1da16dbb8ff48f2df55590c88f609
D <kritantablake@gmail.com>
2022-11-14 10:41:02 +0300

    Create Selector.cs

---
+++ Services/Selector.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace IAS.Services
+{
+	public class Selector
+	{
+        public IEnumerable<string> GetTokens(string text)
+        {
+            int start = -1;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+
+                if (char.IsLetterOrDigit(text[i]))
+                {
+                    if (start == -1)
+                        start = i;
+                }
+                else
+                {
+                    if (start >= 0)
+                    {
+                        yield return GetToken(text, i, start);
+                        start = -1;
+                    }
+                }
+
+            }
+        }
+
+
+        private string GetToken(string text, int i, int start)
+        {
+            return text.Substring(start, i - start).Normalize().ToLowerInvariant();
+        }
+    }
+}
+
