0371b129f36636771096bb9dbff2cc2838e44ce5
D <kritantablake@gmail.com>
2022-11-14 10:40:45 +0300

    add program

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using IAS.DB.Context;
+using IAS.Services;
+using IAS.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+Test(args);
+
+static void Test(string[] args)
+{
+    var host = Host.CreateDefaultBuilder(args)
+                .ConfigureServices(services =>
+                {
+                    services.AddDbContext<DbDocsContext>();
+
+                    services.AddScoped<IDocumentRepository, DocumentRepository>();
+                })
+                .Build();
+
+    Indexing fullTextIndexV1 = new Indexing(host.Services.GetService<DbDocsContext>());
+    fullTextIndexV1.BuildIndex();
+    //host.Services.GetRequiredService<IDocumentRepository>().LoadDocuments();  //load data
+
+}
