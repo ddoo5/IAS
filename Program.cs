9275d1a6c0c02548972026ca7ec690e5112e64a5
D <kritantablake@gmail.com>
2022-11-15 19:04:36 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,14 +1,31 @@
 ﻿using IAS.DB.Context;
 using IAS.Services;
 using IAS.Services.Interfaces;
+using IAS.UI;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-Test(args);
 
-static void Test(string[] args)
+
+UI.ConfigureConsole();
+UI.Greeting();
+
+while (true)
+{
+    try
+    {
+        Starer(args);
+    }
+    catch (Exception a)
+    {
+        Console.WriteLine(a); //todo add ui
+    }
+}
+
+
+static void Starer(string[] args)
 {
     var host = Host.CreateDefaultBuilder(args)
                 .ConfigureServices(services =>
@@ -19,8 +36,33 @@ static void Test(string[] args)
                 })
                 .Build();
 
-    Indexing fullTextIndexV1 = new Indexing(host.Services.GetService<DbDocsContext>());
-    fullTextIndexV1.BuildIndex();
-    //host.Services.GetRequiredService<IDocumentRepository>().LoadDocuments();  //load data
+    UI.ChooseVariants();
+    int variants = Convert.ToInt32(Console.ReadLine());
 
+    switch (variants)
+    {
+        case 1:
+            Console.WriteLine("Started"); //todo add ui
+
+            host.Services.GetRequiredService<IDocumentRepository>().LoadDocuments();
+            break;
+        case 2:
+            Console.WriteLine("Started"); //todo add ui
+
+            Indexing fullTextIndexV1 = new Indexing(host.Services.GetService<DbDocsContext>());
+            fullTextIndexV1.BuildIndex();  //indexing
+            break;
+        case 3:
+
+            break;
+        case 4:
+
+            break;
+        case 5:
+
+            break;
+        default:
+            UI.WrongChoose();
+            break;
+    }
 }
